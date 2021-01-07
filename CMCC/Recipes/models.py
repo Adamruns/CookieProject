7a10bc9c575872d4eb1e914474364da7a3984648
Adam Smith <adam.smith@greermiddlecollege.org>
2021-01-07 12:12:37 -0500

    Initializing

---
+++ CMCC/Recipes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    yield_amount = models.IntegerField()
+    units = models.CharField(max_length=255)
+    prep_time = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    directions = models.TextField()
