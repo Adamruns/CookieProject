dfd9af5d4e7036f3613b5ca0dacff8c0fb30e37f
Adam Smith <adam.smith@greermiddlecollege.org>
2021-01-13 09:11:46 -0500

    Added new form fields, but they are not posting yet

--- recipes/views.py
+++ recipes/views.py
@@ -8,11 +8,10 @@ from . import models
 
 class IndexView(View):
     def get(self, request):
-        recipes = Recipe.objects.order_by('id')
+        recipes = Recipe.objects.all()
         context = {
             'recipes': recipes,
             'title': 'Cookies',
-            'recipes': ''
         }
         return render(request, 'recipes/index.html', context)
 
@@ -34,9 +33,13 @@ class RecipeAddView(View):
     def post(self, request):
         title = self.request.POST.get('title')
         yield_amount = self.request.POST.get('yield_amount')
+        prep_time = self.request.POST.get('prep_time')
+        ingredients = self.request.POST.get('ingredients')
+        directions = self.request.POST.get('directions')
         print(title, yield_amount)
 
-        recipe = models.Recipe.objects.create(title=title, yield_amount=yield_amount)
+        recipe = models.Recipe.objects.create(title=title, yield_amount=yield_amount, prep_time=prep_time,
+         ingredients=ingredients, directions=directions)
         # recipe.title = title
         # recipe.yield_amount = yield_amount
         # recipe.save()
